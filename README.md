cfcfc5c2c801fc58d2d64826721ae134d30390fd
james <jameswcbray@outlook.com>
2021-12-31 13:11:29 +0000

    created basic project structure and started on search class

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # date_ideas
 get date ideas and suggestions based on various criteria
+
+inputs:
+dates
+location
+meal type
+activity
+budget
---
+++ src/main/java/Search.java
@@ -0,0 +1,19 @@
+package src.main.java;
+
+import java.time.LocalDate;
+
+class criteria {
+    public String keyPhrase;
+    public LocalDate[] date;
+    public String location;
+    
+}
+
+public class Search {
+    
+    public static void main (String[] searchArgs){
+        // searchArgs: <criteria/key pharase>, <date to:from>, <location>
+        // returns obj of results
+
+    } 
+}
